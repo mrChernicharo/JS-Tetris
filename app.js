6423352d422b486cfe42fe3859a9b54b6ec53ffb
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-28 17:06:23 -0300

    tetrominoes defined

--- app.js
+++ app.js
@@ -12,29 +12,53 @@ document.addEventListener("DOMContentLoaded", () => {
   const scoreDisplay = document.querySelector("#score");
   const startBtn = document.querySelector("#start-button");
 
+  // tetromino definitions
   const lTetromino = [
     [1, 2, width + 1, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2, width * 2 + 1],
     [0, width, width + 1, width + 2],
   ];
-
   const tTetromino = [
     [0, 1, 2, width + 1],
     [1, width, width + 1, width * 2 + 1],
     [1, width, width + 1, width + 2],
     [1, width, width + 1, width + 2],
   ];
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+  const iTetromino = [
+    [width, width + 1, width + 2, width + 3],
+    [2, width + 2, width * 2 + 2, width * 3 + 2],
+    [width * 2, width * 2 + 1, width * 2 + 2, width * 2 + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+  ];
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [1, 2, width, width + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [1, 2, width, width + 1],
+  ];
 
-  const tertrominoes = [lTetromino, tTetromino];
+  // tetrominoes array
+  const tertrominoes = [
+    lTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+    zTetromino,
+  ];
+
+  const currentPosition = 4;
 
   function draw() {
-    tTetromino[3].forEach((index) => {
-      squares[index].classList.add("tetromino");
+    tertrominoes[3][3].forEach((index) => {
+      squares[currentPosition + index].classList.add("tetromino");
     });
   }
   draw();
-  console.log(grid);
-  console.log(squares);
-  console.log(lTetromino);
 });
