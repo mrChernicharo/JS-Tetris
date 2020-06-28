69f5a9729fed29ba6d6bbe8352158be5c4497d3a
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-28 16:39:42 -0300

    function draw

--- app.js
+++ app.js
@@ -9,8 +9,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const width = 10;
 
-  let count = 0;
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-button");
 
-  console.log(squares.map((div) => (div.innerText = count++)));
+  const lTetromino = [
+    [1, 2, width + 1, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2, width * 2 + 1],
+    [0, width, width + 1, width + 2],
+  ];
+
+  const tertrominoes = [lTetromino];
+
+  function draw() {
+    lTetromino[2].forEach((index) => {
+      squares[index].classList.add("tetromino");
+    });
+  }
+  draw();
   console.log(grid);
+  console.log(squares);
+  console.log(lTetromino);
 });
