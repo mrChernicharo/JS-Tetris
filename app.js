19560ff1d00dc1919c4229b2c9b5898d25c60a6c
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-28 22:44:29 -0300

    addScore()

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-return-assign */
 /* eslint-disable no-plusplus */
@@ -9,13 +10,14 @@
 let count = 0;
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
-  const squares = Array.from(document.querySelectorAll(".grid div"));
+  let squares = Array.from(document.querySelectorAll(".grid div"));
   const scoreDisplay = document.querySelector("#score");
   const startBtn = document.querySelector("#start-button");
   let timerId;
-  // squares.map((item) => (item.innerText = count++));
+  squares.map((item) => (item.innerText = count++));
   const width = 10;
   let nextRandom = 0;
+  let score = 0;
 
 
   // tetromino definitions
@@ -77,7 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   draw();
 
-  // timerId = setInterval(moveDown, 400);
+  timerId = setInterval(moveDown, 400);
 
   function control(e) {
     if (e.keyCode === 37) {
@@ -111,6 +113,7 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition = 4;
       draw();
       displayShape();
+      addScore();
     }
   }
 
@@ -181,4 +184,22 @@ document.addEventListener("DOMContentLoaded", () => {
       displayShape();
     }
   });
+
+  function addScore() {
+    for (let i = 0; i <= 199; i += width) {
+      const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9];
+
+      if (row.every((index) => squares[index].classList.contains('taken'))) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+        row.forEach((index) => {
+          squares[index].classList.remove('taken');
+          squares[index].classList.remove('tetromino');
+        });
+        const squaresRemoved = squares.splice(i, width);
+        squares = squaresRemoved.concat(squares);
+        squares.forEach((cell) => grid.appendChild(cell));
+      }
+    }
+  }
 });
